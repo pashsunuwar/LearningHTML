7bc08f73bf538bfffd993ac358d0054e2098133b
pashsunuwar <psunuwar@qa.com>
2020-07-21 18:24:57 +0100

    worked on CSS and started JS

---
+++ JavaScript/JS/js.js
@@ -0,0 +1,21 @@
+// document.write("Hello world!!!")
+// document.write("Hello there!")
+// window.alert("Is this annoying?")
+// document.write('Learning JavaScript')
+// var a = "string";
+// let b = "anything it wants to be";
+// const myC = "js";
+
+// function doSomething(){
+//     console.log(bar); // undefined
+//     console.log(foo); // ReferenceError
+//     var bar = 1; 
+//     let foo = 2;
+// }
+
+let a = Array();
+let b= Array(10);
+let c = Array("Tom", "Bob", "Harry");
+let d = Array["Jim", "Bob", "Bam"];
+
+console.log(a, b, c, d);
